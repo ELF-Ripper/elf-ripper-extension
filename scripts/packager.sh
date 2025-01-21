b30c47bda22784a0cd62c07acb546e1834c355f3
Hugo Costa <hugo.mfc.pt@gmail.com>
2025-01-21 18:59:08 +0000

    [Feature] Implemented CI/CD Pipeline for Testing and Packaging the Extension
    - Establishes a CI/CD pipeline triggered by pull requests to the main branch.
    - Automates testing and packaging of the extension.
    
    Scripts (developed to encapsulate functionality):
    - `tester.sh`: Handles testing tasks.
    - `packager.sh`: Manages extension packaging process.
    
    Outcome:
    - If tests pass, the extension is packaged and uploaded as an artifact.

---
+++ scripts/packager.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install all the required dependencies from package.json
+npm install
+
+# Clean the project before installing dependencies
+npm run clean
+
+# Add the 'vsce' package from node_modules to the PATH environment variable
+export PATH=$PATH:/workspace/node_modules/@vscode/vsce
+
+# Package the VSCode extension,
+# It first runs the 'vscode:prepublish' script, which builds the extension using webpack in production mode.
+# The output is a packaged .vsix file, ready to be published or installed
+vsce package --allow-missing-repository
